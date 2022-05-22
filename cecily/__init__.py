fab8cf6dbd12458888ad33a070d51cc3215cc55e
Nick Yu <nickyu42@gmail.com>
2022-05-22 13:26:03 +0200

    Cleanup

--- cecily/__init__.py
+++ cecily/__init__.py
@@ -1,17 +1,12 @@
 import concurrent.futures
 import logging
 import multiprocessing
-import multiprocessing.connection as mc
 import os
-import shutil
-import tempfile
-import threading
 from abc import ABC, abstractmethod
 from concurrent.futures import ProcessPoolExecutor
 from dataclasses import dataclass
 from enum import Enum
 from multiprocessing.managers import SyncManager
-from pathlib import Path
 from queue import Queue
 from typing import Any, Callable, Generic, Iterator, TypeVar, final
 from uuid import UUID, uuid4
@@ -36,6 +31,8 @@ def trace(process_alias, msg, *args, **kwargs):
 
 class Status(Enum):
     READY = 1
+    STARTED = 2
+    DONE = 3
 
 
 @final
@@ -50,17 +47,12 @@ JOB_FINISHED = JobFinished()
 class Job:
     id: UUID
     status: Status
-    socket_file: Path
 
     task_fn: Callable
+    args: list[Any]
+    kwargs: dict[Any]
 
-    listener: mc.Listener | None
-    conns: list[mc.Connection]
-
-    conn_listener: threading.Thread
-
-    send: mc.Connection
-    recv: mc.Connection
+    queue: Queue
 
     def __init__(
         self,
@@ -73,86 +65,25 @@ class Job:
         super().__init__()
         self.id = job_id
         self.status = Status.READY
-        # self.socket_file = socket_file
+
         self.task_fn = task_fn
         self.args = args
         self.kwargs = kwargs
-        self.conns = []
-        self.listener = None
-        self.acceptor_started_event = threading.Event()
-        self.recv, self.send = multiprocessing.Pipe()
 
-        self.q = queue
-
-    # def start_listening(self):
-    #     trace(Alias.OTHER, 'socket path listener: %s', self.socket_file)
-    #     self.listener = mc.Listener(str(self.socket_file), family='AF_UNIX')
-    #
-    #     def acceptor(listener: mc.Listener, conns: list[mc.Connection], started: threading.Event):
-    #         logger.debug('[WORKER] started listener for task id=%s', self.id)
-    #
-    #         started.set()
-    #
-    #         while True:
-    #             conn = listener.accept()
-    #             logger.debug('[WORKER] accepted conn: %s', conn)
-    #             conns.append(conn)
-    #
-    #     # FIXME: kill thread on done
-    #     self.conn_listener = threading.Thread(
-    #         target=acceptor, args=(self.listener, self.conns, self.acceptor_started_event), daemon=True
-    #     )
-    #     self.conn_listener.start()
-
-    # def notify(self, obj):
-    #     for conn in self.conns:
-    #         conn.send(obj)
-
-    def start_work(self) -> Any:
-        trace(Alias.WORKER, 'job starting for id=%s', self.id)
-
-        # started_evt = threading.Event()
-
-        # recv, send = multiprocessing.Pipe()
-
-        # def notifier(started: threading.Event, conn: mc.Connection, job: Job):
-        #     started.set()
-        #
-        #     while not conn.closed:
-        #         # FIXME: this currently exits by closing conn and throwing an exception
-        #         #   preferably this should be handled nicer than dying
-        #         try:
-        #             job.notify(conn.recv())
-        #         finally:
-        #             break
-
-        # threading.Thread(target=notifier, args=(started_evt, self.recv, self), daemon=True).start()
-
-        # XXX: arbitrary wait
-        # started_evt.wait(timeout=15)
-
-        result = self.task_fn(*self.args, **self.kwargs, notifier=self.q)  # noqa
-
-        return result
+        self.queue = queue
 
     def execute(self) -> Any:
-        # self.start_listening()
-        # self.acceptor_started_event.wait()
+        trace(Alias.WORKER, 'job starting for id=%s', self.id)
+        self.status = Status.STARTED
+        result = self.task_fn(*self.args, **self.kwargs, notifier=self.queue)
 
-        result = self.start_work()
-
-        # self.q.close()
-        # self.q.join()
-
-        self.q.put(JOB_FINISHED)
+        self.queue.put(JOB_FINISHED)
+        self.status = Status.DONE
 
         # XXX: don't know if this is necessary
         # forcefully delete weakref to queue object
-        del self.q
+        del self.queue
 
-        # self.listener.close()
-        # self.recv.close()
-        # self.send.close()
         return result
 
 
@@ -167,16 +98,6 @@ class CecilyFuture(Generic[RT]):
         if not self._future.running:
             return
 
-        # with mc.Client(str(self.socket_file), family='AF_UNIX') as client:
-        #     # try:
-        #     #     def done_callback(_future):
-        #     #         raise RuntimeError
-        #     #
-        #     #     self._future.add_done_callback(done_callback)
-        #
-        #     while not client.closed and self._future.running():
-        #         yield client.recv()
-
         while True:
             v = self.queue.get()
 
@@ -184,9 +105,6 @@ class CecilyFuture(Generic[RT]):
                 break
 
             yield v
-
-        # finally:
-        #     pass
 
     def result(self) -> Any:
         return self._future.result()
@@ -204,9 +122,7 @@ class CecilyTask(Callable, ABC):
 
 def worker(task_fn, job_id, queue, args, kwargs):
     trace(Alias.WORKER, 'init new job with task_fn=%s id=%s', task_fn.__name__, job_id)
-
     job = Job(job_id, queue, task_fn, args, kwargs)
-
     return job.execute()
 
 
@@ -221,19 +137,12 @@ class Task:
         # create id
         job_id = uuid4()
 
-        # create pub/sub conn
-        # temp_socket_file: Path = self.app_ref.sock_dir / str(job_id)
-        # temp_socket_file = temp_socket_file.with_suffix('.sock')
-
         queue_proxy = self.app_ref.manager.Queue()
-        # self.app_ref.manager.register(job_id, queue)
 
         future = self.app_ref.executor.submit(
             worker,
             self.task_fn,
             job_id,
-            # temp_socket_file,
-            # self.task_fn.__name__,
             queue_proxy,
             args,
             kwargs,
@@ -243,15 +152,8 @@ class Task:
 
 
 class Cecily:
-    executor: ProcessPoolExecutor
-    sock_dir: Path
-
-    manager_sock: Path
-
-    serialized_tasks: list[bytes]
-
     spawned: bool
-
+    executor: ProcessPoolExecutor
     manager: SyncManager
 
     def __init__(self, max_workers: int | None = None) -> None:
@@ -267,9 +169,7 @@ class Cecily:
         trace(Alias.MAIN, 'creating app')
         self.spawned = False
         self.executor = ProcessPoolExecutor(max_workers)
-        self.sock_dir = Path(tempfile.mkdtemp())
 
-        self.deferred_functions = []
         self.manager = SyncManager(address='manager.sock')
 
     def start(self):
@@ -288,16 +188,12 @@ class Cecily:
 
         fn.apply = Task(self, fn)
 
-        # TODO: no duplicate registers
-        self.deferred_functions.append(fn)
-
         return fn
 
     def close(self):
         logger.debug('[MAIN] shutting down app')
-        self.manager.shutdown()
 
         self.executor.shutdown(cancel_futures=True)
+        self.manager.shutdown()
 
-        shutil.rmtree(self.sock_dir)
         logger.debug('[MAIN] shutdown complete')
